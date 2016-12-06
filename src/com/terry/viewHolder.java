835030afd957536a8ecb2f529ff5143f5ff0afc2
QHonhu <3240733486@qq.com>
2016-12-06 09:31:36 +0800

    这个是音乐播放器的制作，点击按钮以后可以实现音乐的播放

---
+++ src/com/terry/viewHolder.java
@@ -0,0 +1,10 @@
+package com.terry;
+
+public class viewHolder {
+
+	public static Button start;
+	public static Button stop;
+	public static Button pause;
+	public static Button next;
+	public static Button last;
+}
